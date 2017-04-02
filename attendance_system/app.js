c6ad336266f2058ea97eb5272d758961e4eacc3b
neelansh <neelansh5@gmail.com>
2017-04-02 16:07:36 +0530

    favicon added

--- attendance_system/app.js
+++ attendance_system/app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -103,7 +103,8 @@ app.use(flash());
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.errors = null;
+  res.locals.error = req.flash('error');
+  res.locals.errors_validator = null;
   res.locals.user = req.user || null;
   next();
 });
